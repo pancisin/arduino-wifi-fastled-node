9bbd5a928516d6272d58875a1152c44838b40cde
Patrik Pancisin <patrik.pancisin.extern@rwe.com>
2025-07-28 22:04:47 +0200

    chore: move header files to separate folder

--- color_utils.h
+++
@@ -1,8 +0,0 @@
-#ifndef COLOR_UTILS_INCLUDE
-#define COLOR_UTILS_INCLUDE
-
-void shift_left(struct CRGB *arr, int numToMove);
-void fill_point_gradient(struct CRGB *targetArray, int numToFill,
-                         CRGB &color1, const CRGB &color2);
-
-#endif
